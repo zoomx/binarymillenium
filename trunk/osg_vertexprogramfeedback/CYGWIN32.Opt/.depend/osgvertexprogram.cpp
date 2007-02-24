8b2e6760a42ba897eea40ba2d03ac8a03f6fa033
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2007-02-24 19:15:36 +0000

    Initial import of modified osg vertexprogram to take current frame buffer and use it as cubemap texture.

---
+++ trunk/osg_vertexprogramfeedback/CYGWIN32.Opt/.depend/osgvertexprogram.cpp
@@ -0,0 +1,254 @@
+osgvertexprogram.o: ../osgvertexprogram.cpp \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/sstream \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/istream \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/ios \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/iosfwd \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/c++config.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/os_defines.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/c++locale.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/clocale \
+  /usr/include/locale.h /usr/include/_ansi.h /usr/include/newlib.h \
+  /usr/include/sys/config.h /usr/include/machine/ieeefp.h \
+  /usr/include/cygwin/config.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cstring \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cstddef \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/stddef.h \
+  /usr/include/string.h /usr/include/sys/reent.h /usr/include/_ansi.h \
+  /usr/include/sys/_types.h /usr/include/sys/lock.h \
+  /usr/include/sys/string.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cstdio \
+  /usr/include/stdio.h /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/stdarg.h \
+  /usr/include/sys/types.h /usr/include/machine/_types.h \
+  /usr/include/machine/types.h /usr/include/sys/features.h \
+  /usr/include/cygwin/types.h /usr/include/sys/sysmacros.h \
+  /usr/include/stdint.h /usr/include/endian.h /usr/include/sys/stdio.h \
+  /usr/include/sys/cdefs.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/c++io.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/gthr.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/gthr-default.h \
+  /usr/include/pthread.h /usr/include/signal.h /usr/include/sys/signal.h \
+  /usr/include/cygwin/signal.h /usr/include/sched.h /usr/include/time.h \
+  /usr/include/machine/time.h /usr/include/cygwin/time.h \
+  /usr/include/unistd.h /usr/include/sys/unistd.h /usr/include/getopt.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cctype \
+  /usr/include/ctype.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stringfwd.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/postypes.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cwchar \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/ctime \
+  /usr/include/wchar.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/functexcept.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/exception_defines.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/exception \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/char_traits.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_algobase.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/climits \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/limits.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/syslimits.h \
+  /usr/include/limits.h /usr/include/features.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cstdlib \
+  /usr/include/stdlib.h /usr/include/machine/stdlib.h \
+  /usr/include/alloca.h /usr/include/cygwin/stdlib.h \
+  /usr/include/cygwin/wait.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/new \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_pair.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/type_traits.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_iterator_base_types.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_iterator_base_funcs.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/concept_check.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_iterator.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/debug/debug.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cassert \
+  /usr/include/assert.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/localefwd.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/ios_base.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/atomicity.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/atomic_word.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/locale_classes.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/string \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/memory \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/allocator.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/c++allocator.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/ext/new_allocator.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_construct.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_uninitialized.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_raw_storage_iter.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/limits \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_function.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/basic_string.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/algorithm \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_algo.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_heap.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_tempbuf.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/basic_string.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/streambuf \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/streambuf.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/basic_ios.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/streambuf_iterator.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/locale_facets.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/cwctype \
+  /usr/include/wctype.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/ctype_base.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/ctype_inline.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/codecvt.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/time_members.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/messages_members.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/basic_ios.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/istream.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/locale \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/locale_facets.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/typeinfo \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/ostream \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/ostream.tcc \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/sstream.tcc \
+  ../../../include/osg/Vec3 ../../../include/osg/Vec3f \
+  ../../../include/osg/Vec2f ../../../include/osg/Math \
+  /usr/include/math.h ../../../include/osg/Vec4 \
+  ../../../include/osg/Vec4f ../../../include/osg/Quat \
+  ../../../include/osg/Export ../../../include/osg/Vec3d \
+  ../../../include/osg/Vec2d ../../../include/osg/Vec4d \
+  ../../../include/osg/Matrix ../../../include/osg/Matrixd \
+  ../../../include/osg/Object ../../../include/osg/Referenced \
+  /usr/local/include/OpenThreads/ScopedLock \
+  /usr/local/include/OpenThreads/Mutex \
+  /usr/local/include/OpenThreads/Exports ../../../include/osg/CopyOp \
+  ../../../include/osg/ref_ptr ../../../include/osg/Matrixf \
+  ../../../include/osg/ShapeDrawable ../../../include/osg/Drawable \
+  ../../../include/osg/BoundingBox \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/float.h \
+  ../../../include/osg/Shape ../../../include/osg/Plane \
+  ../../../include/osg/BoundingSphere \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/vector \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_vector.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_bvector.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/vector.tcc \
+  ../../../include/osg/Array ../../../include/osg/Vec2 \
+  ../../../include/osg/Vec4ub ../../../include/osg/Vec2s \
+  ../../../include/osg/Vec3s ../../../include/osg/Vec4s \
+  ../../../include/osg/Vec2b ../../../include/osg/Vec3b \
+  ../../../include/osg/Vec4b ../../../include/osg/GL \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/GL/gl.h \
+  ../../../include/osg/BufferObject ../../../include/osg/buffered_value \
+  ../../../include/osg/DisplaySettings ../../../include/osg/PrimitiveSet \
+  ../../../include/osg/State ../../../include/osg/StateSet \
+  ../../../include/osg/StateAttribute \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/utility \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_relops.h \
+  ../../../include/osg/Uniform ../../../include/osg/GL2Extensions \
+  ../../../include/osg/Program \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/map \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_tree.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/cpp_type_traits.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_map.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_multimap.h \
+  ../../../include/osg/Shader \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/set \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_set.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_multiset.h \
+  ../../../include/osg/FrameStamp ../../../include/osg/Polytope \
+  ../../../include/osg/fast_back_stack ../../../include/osg/Viewport \
+  ../../../include/osg/Geometry ../../../include/osg/Geode \
+  ../../../include/osg/Node ../../../include/osg/NodeCallback \
+  ../../../include/osg/NodeVisitor ../../../include/osg/Transform \
+  ../../../include/osg/Group ../../../include/osg/Material \
+  ../../../include/osg/Depth ../../../include/osg/CullFace \
+  ../../../include/osg/TexMat ../../../include/osg/TexGen \
+  ../../../include/osg/TexEnv ../../../include/osg/TexEnvCombine \
+  ../../../include/osg/TextureCubeMap ../../../include/osg/Texture \
+  ../../../include/osg/Image ../../../include/osg/GraphicsContext \
+  ../../../include/osg/GraphicsThread \
+  /usr/local/include/OpenThreads/Thread \
+  /usr/local/include/OpenThreads/Barrier \
+  /usr/local/include/OpenThreads/Condition \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/list \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_list.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/list.tcc \
+  ../../../include/osg/VertexProgram ../../../include/osgDB/Registry \
+  ../../../include/osg/ArgumentParser \
+  ../../../include/osgDB/DynamicLibrary ../../../include/osgDB/Export \
+  ../../../include/osgDB/ReaderWriter \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/deque \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/stl_deque.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/deque.tcc \
+  ../../../include/osgDB/DotOsgWrapper ../../../include/osgDB/Input \
+  ../../../include/osgDB/FieldReaderIterator ../../../include/osgDB/Field \
+  ../../../include/osgDB/FieldReader ../../../include/osgDB/Output \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/fstream \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin/bits/basic_file.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/bits/fstream.tcc \
+  ../../../include/osgDB/DatabasePager ../../../include/osg/PagedLOD \
+  ../../../include/osg/LOD ../../../include/osgDB/SharedStateManager \
+  ../../../include/osgDB/ReadFile ../../../include/osgDB/WriteFile \
+  ../../../include/osgUtil/SmoothingVisitor \
+  ../../../include/osgUtil/Export ../../../include/osgUtil/Optimizer \
+  ../../../include/osg/Texture2D ../../../include/osgProducer/Viewer \
+  ../../../include/osg/ApplicationUsage \
+  ../../../include/osg/AnimationPath ../../../include/osg/observer_ptr \
+  ../../../include/osgUtil/IntersectVisitor \
+  ../../../include/osg/LineSegment \
+  ../../../include/osgGA/GUIActionAdapter ../../../include/osgGA/Export \
+  ../../../include/osgGA/GUIEventHandler \
+  ../../../include/osgGA/GUIEventAdapter \
+  ../../../include/osgGA/GUIEventHandlerVisitor \
+  ../../../include/osgGA/EventVisitor ../../../include/osg/Billboard \
+  ../../../include/osg/Switch ../../../include/osg/LightSource \
+  ../../../include/osg/Light ../../../include/osg/Projection \
+  ../../../include/osg/OccluderNode \
+  ../../../include/osg/ConvexPlanarOccluder \
+  ../../../include/osg/ConvexPlanarPolygon \
+  ../../../include/osgGA/EventQueue ../../../include/osg/Timer \
+  ../../../include/osgGA/KeySwitchMatrixManipulator \
+  ../../../include/osgGA/MatrixManipulator \
+  ../../../include/osg/CoordinateSystemNode \
+  ../../../include/osgUtil/SceneView \
+  ../../../include/osg/CollectOccludersVisitor \
+  ../../../include/osg/CullStack ../../../include/osg/CullingSet \
+  ../../../include/osg/ShadowVolumeOccluder \
+  ../../../include/osg/CullSettings ../../../include/osg/ClearNode \
+  ../../../include/osg/CameraNode ../../../include/osg/ColorMask \
+  ../../../include/osgUtil/CullVisitor ../../../include/osg/Notify \
+  ../../../include/osgUtil/StateGraph ../../../include/osgUtil/RenderLeaf \
+  ../../../include/osgUtil/RenderStage \
+  ../../../include/osg/FrameBufferObject \
+  ../../../include/osgUtil/RenderBin \
+  ../../../include/osgUtil/PositionalStateContainer \
+  ../../../include/osgProducer/OsgCameraGroup \
+  /usr/local/Producer/include/Producer/CameraConfig \
+  /usr/local/Producer/include/Producer/Export \
+  /usr/local/Producer/include/Producer/Referenced \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/iostream \
+  /usr/local/Producer/include/Producer/Types \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/windows.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/windef.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/winnt.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/winerror.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/basetsd.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/pshpack4.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/poppack.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/wincon.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/winbase.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/wingdi.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/winuser.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/winnls.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/winver.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/winnetwk.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/winreg.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api/winsvc.h \
+  /usr/local/Producer/include/Producer/VisualChooser \
+  /usr/local/Producer/include/Producer/RenderSurface \
+  /usr/local/Producer/include/Producer/RefOpenThreads \
+  /usr/local/Producer/include/Producer/Block \
+  /usr/local/Producer/include/Producer/Camera \
+  /usr/local/Producer/include/Producer/Math \
+  /usr/local/Producer/include/Producer/Timer \
+  /usr/local/Producer/include/Producer/PipeTimer \
+  /usr/local/Producer/include/Producer/CameraGroup \
+  /usr/local/Producer/include/Producer/InputArea \
+  ../../../include/osgProducer/OsgSceneHandler \
+  ../../../include/osgUtil/Statistics ../../../include/osgProducer/Export \
+  ../../../include/osgProducer/GraphicsContextImplementation \
+  ../../../include/osgProducer/KeyboardMouseCallback \
+  /usr/local/Producer/include/Producer/KeyboardMouse \
+  /usr/local/Producer/include/Producer/BlockingQueue \
+  /usr/local/Producer/include/Producer/Keyboard \
+  ../../../include/osgProducer/ViewerEventHandler
