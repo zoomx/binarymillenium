2e1d6b71ff7e80f6cfad9ec3c1e8c31fe4bfb555
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2007-08-25 13:57:39 +0000

    Made location of xml file less specific to my system, but it's still not right.

--- trunk/facedetect/compile.sh
+++
@@ -1,4 +0,0 @@
-gcc camshiftdemo.c -L/home/lucasw/other/install/lib/ -lcv -lhighgui -I/home/lucasw/other/install/include/opencv -o camshift
-
-gcc facedetect.c -L/home/lucasw/other/install/lib/ -lcv -lhighgui -I/home/lucasw/other/install/include/opencv -o facedetect
-
