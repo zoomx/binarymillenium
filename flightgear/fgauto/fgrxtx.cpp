cfe9fc1cc415015d4e68e62d11df613855a3904b
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2008-02-03 08:00:41 +0000

    This seems to be a working autopilot framework now.
    the pattern is to get the fdm, get the ctrl, and then modify flight 
    controls within the control structure and send it back.  It causes some 
    stuttering at 5 Hz, probably because environmental things like air 
    pressure are being reset, faster updates should help though.
    
    I need to get attitude rates in body frame, either with my own transform 
    or convincing a fg developer to add it.

--- flightgear/fgauto/fgrxtx.cpp
+++ flightgear/fgauto/fgrxtx.cpp
@@ -7,6 +7,7 @@
  */
 
 #include "net_fdm.hxx"
+#include "net_ctrls.hxx"
 
 #include <stdio.h>
 #include <sys/socket.h>
@@ -17,7 +18,22 @@
 #include <netinet/in.h>
 #include <errno.h>
 
-//#define BUFFSIZE 408
+#define M_PI 3.14592
+
+struct known_state {
+	double longitude;
+	double latitude;
+	double altitude;
+
+	/// not sure if thiese are in body frame
+	float phidot;
+	float thetadot;
+	float psidot;
+
+	float A_X_pilot;
+	float A_Y_pilot;
+	float A_Z_pilot;
+};
 
 static void htond (double &x)   
 {
@@ -47,6 +63,65 @@ static void htonf (float &x)
 //	}
 }
 
+void FGProps2NetCtrls( FGNetCtrls *net)
+{
+	int i = 0;
+
+	// convert to network byte order
+	net->version = htonl(net->version);
+	htond(net->aileron);
+	htond(net->elevator);
+	htond(net->rudder);
+	htond(net->aileron_trim);
+	htond(net->elevator_trim);
+	htond(net->rudder_trim);
+	htond(net->flaps);
+	net->flaps_power = htonl(net->flaps_power);
+	net->flap_motor_ok = htonl(net->flap_motor_ok);
+
+	net->num_engines = htonl(net->num_engines);
+	for ( i = 0; i < FGNetCtrls::FG_MAX_ENGINES; ++i ) {
+		net->master_bat[i] = htonl(net->master_bat[i]);
+		net->master_alt[i] = htonl(net->master_alt[i]);
+		net->magnetos[i] = htonl(net->magnetos[i]);
+		net->starter_power[i] = htonl(net->starter_power[i]);
+		htond(net->throttle[i]);
+		htond(net->mixture[i]);
+		net->fuel_pump_power[i] = htonl(net->fuel_pump_power[i]);
+		htond(net->prop_advance[i]);
+		htond(net->condition[i]);
+		net->engine_ok[i] = htonl(net->engine_ok[i]);
+		net->mag_left_ok[i] = htonl(net->mag_left_ok[i]);
+		net->mag_right_ok[i] = htonl(net->mag_right_ok[i]);
+		net->spark_plugs_ok[i] = htonl(net->spark_plugs_ok[i]);
+		net->oil_press_status[i] = htonl(net->oil_press_status[i]);
+		net->fuel_pump_ok[i] = htonl(net->fuel_pump_ok[i]);
+	}
+
+	net->num_tanks = htonl(net->num_tanks);
+	for ( i = 0; i < FGNetCtrls::FG_MAX_TANKS; ++i ) {
+		net->fuel_selector[i] = htonl(net->fuel_selector[i]);
+	}
+
+	net->cross_feed = htonl(net->cross_feed);
+	htond(net->brake_left);
+	htond(net->brake_right);
+	htond(net->copilot_brake_left);
+	htond(net->copilot_brake_right);
+	htond(net->brake_parking);
+	net->gear_handle = htonl(net->gear_handle);
+	net->master_avionics = htonl(net->master_avionics);
+	htond(net->wind_speed_kt);
+	htond(net->wind_dir_deg);
+	htond(net->turbulence_norm);
+	htond(net->temp_c);
+	htond(net->press_inhg);
+	htond(net->hground);
+	htond(net->magvar);
+	net->icing = htonl(net->icing);
+	net->speedup = htonl(net->speedup);
+	net->freeze = htonl(net->freeze);
+}
 
 void FGNetFDM2Props( FGNetFDM *net ) {
     unsigned int i;
@@ -55,9 +130,9 @@ void FGNetFDM2Props( FGNetFDM *net ) {
         // Convert to the net buffer from network format
         net->version = ntohl(net->version);
 
-        htond(net->longitude);
-        htond(net->latitude);
-        htond(net->altitude);
+        htond(net->longitude);  // use
+        htond(net->latitude);   // use 
+        htond(net->altitude);   // use
         htonf(net->agl);
         htonf(net->phi);
         htonf(net->theta);
@@ -65,9 +140,9 @@ void FGNetFDM2Props( FGNetFDM *net ) {
         htonf(net->alpha);
         htonf(net->beta);
 
-        htonf(net->phidot);
-        htonf(net->thetadot);
-        htonf(net->psidot);
+        htonf(net->phidot);		// use
+        htonf(net->thetadot);	// use
+        htonf(net->psidot); 	// use
         htonf(net->vcas);
         htonf(net->climb_rate);
         htonf(net->v_north);
@@ -77,9 +152,9 @@ void FGNetFDM2Props( FGNetFDM *net ) {
         htonf(net->v_wind_body_east);
         htonf(net->v_wind_body_down);
 
-        htonf(net->A_X_pilot);
-        htonf(net->A_Y_pilot);
-        htonf(net->A_Z_pilot);
+        htonf(net->A_X_pilot);	// use
+        htonf(net->A_Y_pilot);  // use
+        htonf(net->A_Z_pilot);	// use
 
         htonf(net->stall_warning);
         htonf(net->slip_deg);
@@ -89,14 +164,19 @@ void FGNetFDM2Props( FGNetFDM *net ) {
 
 int main(void)
 {
-	int sock;
-	struct sockaddr_in echoclient,from;
-	//char buffer[BUFFSIZE];
+	int sock, sock2;
+	struct sockaddr_in echoclient,from, ctrlclient, ctrlinclient;
 	unsigned int echolen, clientlen;
 	int received = 0;
 
 	FGNetFDM buf;
 	char* buffer = (char*)&buf;
+
+	FGNetCtrls bufctrl;
+	char* buffer_ctrl = (char*)&bufctrl; 
+
+	struct known_state state; 
+
 
 	/* Create the UDP socket */
 	if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
@@ -105,16 +185,30 @@ int main(void)
 		return -1;
 	}
 
-	/* Construct the server sockaddr_in structure */
+	if ((sock2 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+		std::cerr << "Failed to create socket" << std::endl;
+		perror("socket");
+		return -1;
+	}
+
+	/// the port where net fdm is received
 	memset(&echoclient, 0, sizeof(echoclient));       /* Clear struct */
 	echoclient.sin_family = AF_INET;                  /* Internet/IP */
-	//echoclient.sin_addr.s_addr = inet_addr("127.0.0.1");  /* IP address */
-	echoclient.sin_addr.s_addr = (inet_addr("192.168.1.101"));  /* IP address */
+	echoclient.sin_addr.s_addr = inet_addr("127.0.0.1");  /* IP address */
 	echoclient.sin_port = htons(5500);       /* server port */
 
-/// receive net_fdm over udp
-///
-//
+	/// the port where the modified net ctrl is sent to flightgear
+	memset(&ctrlclient, 0, sizeof(ctrlclient));       /* Clear struct */
+	ctrlclient.sin_family = AF_INET;                  /* Internet/IP */
+	ctrlclient.sin_addr.s_addr = inet_addr("127.0.0.1");  /* IP address */
+	ctrlclient.sin_port = htons(5600);       /* server port */
+
+	/// this is the port where the net ctrl comes from flight gear
+	memset(&ctrlinclient, 0, sizeof(ctrlinclient));       /* Clear struct */
+	ctrlinclient.sin_family = AF_INET;                  /* Internet/IP */
+	ctrlinclient.sin_addr.s_addr = inet_addr("127.0.0.1");  /* IP address */
+	ctrlinclient.sin_port = htons(5700);       /* server port */
+
 	int length = sizeof(buf);
 
 	std::cout << "packet length should be " << length << " bytes" << std::endl;
@@ -124,40 +218,19 @@ int main(void)
 	char errorbuf[100];
 
 	/// need this?
-	int ret = bind(sock, (struct sockaddr*)&echoclient, sizeof(echoclient) );
+	int ret  = bind(sock,  (struct sockaddr*)&echoclient, sizeof(echoclient) );
+	int ret2 = bind(sock2, (struct sockaddr*)&ctrlinclient, sizeof(ctrlinclient) );
 	
-	std::cout << "bind " << ret << std::endl;
+	std::cout << "bind " << ret << " " << ret2 << std::endl;
 
+	double time = 0.0; 
 
 	while(1) {
-/*
-		received = sendto(sock, buffer, BUFFSIZE, 0,
-			(struct sockaddr *) &echoclient, sizeof(echoclient));
-
-			std::cout << "sent " << received << " bytes " << errno <<std::endl;
-	
-		perror("sendto");
-		usleep(1000000);
-*/
 		// I think this clobbers echoclient
+		/// receive net_fdm over udp
 		received = recvfrom(sock, buffer, sizeof(buf), 0,
 						(struct sockaddr *) &from,
 						&echolen);
-			
-		if (received < 0) {
-			perror("recvfrom");
-	//	} else if (received != echolen) {
-	//		std::cerr << "wrong sized packet " << received << std::endl;
-		} else {
-
-			/// copy buffer into fdm struct
-
-			FGNetFDM2Props( &buf);
-			std::cout << "altitude " << buf.altitude << " bytes " << std::endl;
-		}
-		usleep(1000);
-
-	}
 	
 /// -extract accelerations A_X_pilot etc, add noise
 /// - are phidot, thetadot, psidot in body frame? Probably 
@@ -168,5 +241,73 @@ int main(void)
 
 /// and that's all we'll know
 
+		FGNetFDM2Props( &buf);
+
+		state.longitude = buf.longitude;
+		state.latitude = buf.latitude;
+		state.altitude = buf.altitude;
+
+		state.phidot = buf.phidot;
+		state.thetadot = buf.thetadot;
+		state.psidot = buf.psidot;
+
+		state.A_X_pilot = buf.A_X_pilot;
+		state.A_Y_pilot = buf.A_Y_pilot;
+		state.A_Z_pilot = buf.A_Z_pilot;
+
+
+		if (received < 0) {
+			perror("recvfrom");
+	//	} else if (received != echolen) {
+	//		std::cerr << "wrong sized packet " << received << std::endl;
+		} else {
+
+			/// get the ctrls that are properly filled out with environmental
+			/// data, so we don't clobber that when we modify the flap positions
+			received = recvfrom(sock2, buffer_ctrl, sizeof(bufctrl), 0,
+						(struct sockaddr *) &ctrlinclient,
+						&echolen);
+
+			FGProps2NetCtrls(&bufctrl);
+			/// copy buffer into fdm struct
+
+			std::cout << "altitude " << buf.altitude << " bytes " << std::endl;
+	
+			/// now send some ctrls out, just anything random to show it works;
+			float val = -state.phidot;
+			if (val > 1.0) val = 1.0;
+			if (val <-1.0) val =-1.0;
+			bufctrl.elevator = val;
+
+			val = -state.thetadot;
+			if (val > 1.0) val = 1.0;
+			if (val <-1.0) val =-1.0;
+			bufctrl.rudder = val;
+		
+			val = -state.psidot;
+			if (val > 1.0) val = 1.0;
+			if (val <-1.0) val =-1.0;
+			//bufctrl.elevation = val;
+
+			received = sendto(sock, buffer_ctrl, sizeof(bufctrl), 0,
+			(struct sockaddr *) &ctrlclient, sizeof(ctrlclient));
+
+			//std::cout << "sent " << val << " " <<std::endl;
+			std::cout << "sent " << 
+							state.phidot << " " <<
+							state.thetadot << " " <<
+							state.psidot << " " <<
+									std::endl;
+			
+			if (received <0) {
+				perror("sendto");
+			}
+
+			time += 0.1;	
+		
+		}
+
+		usleep(1000);
+	}
 return 0;
 }
