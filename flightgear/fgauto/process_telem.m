3cf0d6c815561c3b25f0cbebb99e0873308bd610
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2008-02-12 16:21:49 +0000

    Added max pitch limit (should be minimum glide angle) and min pitch limit (don't dive too steeply), Also made gps derived telemetry update at 1 Hz.
    Near the ground things get really jittery, a faster update rate may help there.

--- flightgear/fgauto/process_telem.m
+++ flightgear/fgauto/process_telem.m
@@ -8,37 +8,49 @@ f = f';
 
 
 len =size(f,2)
-t = [1:len]/10;
+
+l = round(len*0.9);
+
+t = [1:l]/10;
 r2d = 180/pi;
-p = f(4,:)*r2d;
-q = f(5,:)*r2d;
-dq = f(10,:);
-iq = f(11,:);
-r = f(6,:)*r2d;
 
-err_head = f(12,:);
-derr_head = f(13,:);
-ierr_head = f(14,:);
+p = f(4,1:l)*r2d;
+q = f(5,1:l)*r2d;
+r = f(6,1:l)*r2d;
 
-elev = f(7,:);
-rud = f(8,:);
-ail = f(9,:);
+err_head = f(12,1:l);
+derr_head = f(13,1:l);
+ierr_head = f(14,1:l);
 
-pitch  = f(15,:);
-dpitch = f(16,:);
-ipitch = f(17,:);
+elev = f(7,1:l);
+rud = f(8,1:l);
+ail = f(9,1:l);
 
-tpitch = f(18,:);
-speed = f(19,:);
+dq = f(10,1:l);
+iq = f(11,1:l);
+
+pitch  = f(15,1:l);
+dpitch = f(16,1:l);
+ipitch = f(17,1:l);
+
+tpitch = f(18,1:l);
+speed = f(19,1:l);
+
+tdx = f(20,1:l);
+tdy = f(21,1:l);
 
 figure(1);
-plot(f(1,:), f(2,:));
-ylabel('long vs. lat');
+plot(tdx, tdy);
+ylabel('tdx vs. tdy');
 
 figure(2),
-plot(f(3,:));
-ylabel('altitude');
+subplot(2,1,1),plot(t,f(3,1:l));
+ylabel('altitude meters');
+xlabel('time seconds');
 
+subplot(2,1,2),plot(t,speed);
+legend('speed');
+ylabel('meters/second');
 
 figure(3),
 plot(t,p, t,q,t,r);
@@ -46,18 +58,17 @@ legend('p','q','r');
 
 figure(4),
 factor = 100; % max(abs(q))/ max(abs(elev));
-subplot(2,1,1),plot(t,elev*factor, t, q,t,dq*500, t, iq*50);
+subplot(2,1,1),plot(t,elev*factor, t, q,t,dq*500, t, iq*10);
 ylabel('radians/sec, or other units');
 legend('elevator','q', 'dq','iq');
-subplot(2,1,2),plot(t, elev, t,pitch, t, tpitch, t, dpitch*10, t, ipitch);
+subplot(2,1,2),plot(t, elev, t,pitch, t, tpitch, t, dpitch*50, t, ipitch/5);
 legend('elev', 'gps derived pitch', 'tpitch', 'dpitch', 'ipitch');
 
 figure(5),
 plot(t,rud*100, t,err_head, t, derr_head*10, t, ierr_head/10);
 ylabel('degrees');
-legend('rudder', 'error heading', 'derror heading', 'ierr_head');
+%legend('rudder', 'error heading', 'derror heading', 'ierr head');
 
-figure(6),
-plot(t,speed);
-legend('speed');
-ylabel('meters/second');
+
+
+
