de99e27a46260834ff30a756406eb1ef87332c9a
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2008-02-23 07:04:46 +0000

    Commenting out the winds I think fixed the glitchiness at low altitudes- maybe flightgear does have winds at low altitudes? (can find out easily enough)
    The autopilot needs a better scheme than circling around the target- it needs to line up and actually attempt to land, flying straight.

--- flightgear/fgauto/process_telem.m
+++ flightgear/fgauto/process_telem.m
@@ -9,67 +9,79 @@ f = f';
 
 len =size(f,2)
 
+s = 20;
 l = round(len*0.9);
+range = [s:l];
 
-t = [10:l]/20;
+t = [range]/20;
 r2d = 180/pi;
 
-long = f(1,10:l);
-lat = f(2,10:l);
-alt = f(3,10:l);
+long = f(1,range);
+lat = f(2,range);
+alt = f(3,range);
 
-p = f(4,10:l)*r2d;
-q = f(5,10:l)*r2d;
-r = f(6,10:l)*r2d;
+p = f(4,range)*r2d;
+q = f(5,range)*r2d;
+r = f(6,range)*r2d;
 
-err_head = f(12,10:l);
-derr_head = f(13,10:l);
-ierr_head = f(14,10:l);
+err_head = f(12,range);
+derr_head = f(13,range);
+ierr_head = f(14,range);
 
 % control surfaces
-elev = f(7,10:l);
-rud = f(8,10:l);
-ail = f(9,10:l);
+elev = f(7,range);
+rud = f(8,range);
+ail = f(9,range);
 
-dq = f(10,10:l);
-iq = f(11,10:l);
+dq = f(10,range);
+iq = f(11,range);
 
-epitch  = f(15,10:l);
-dpitch = f(16,10:l);
-ipitch = f(17,10:l);
+epitch  = f(15,range);
+dpitch = f(16,range);
+ipitch = f(17,range);
 
-tpitch = f(18,10:l);
-speed = f(19,10:l);
+tpitch = f(18,range);
+speed = f(19,range);
 
-tdx = f(20,10:l);
-tdy = f(21,10:l);
+tdx = f(20,range);
+tdy = f(21,range);
 
-wind = f(22,10:l);
-wind_dir = f(23,10:l);
-press = f(24,10:l);
+wind = f(22,range);
+wind_dir = f(23,range);
+press = f(24,range);
 
-dr = f(25,10:l);
-ir = f(26,10:l);
+dr = f(25,range);
+ir = f(26,range);
 
-pitch = f(27,10:l);
+pitch = f(27,range);
+
+
+
+y = (lat-lat(end))*6e6;
+y2 = (long-long(end))*6e6.*cos(lat);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+if (0)
 figure(1);
 %instead of lat(end) should use coords of actual target, and make an x where the target is
-y = (lat-lat(end))*6e6;
 subplot(2,1,1),plot(t,y);
 ylabel('dist meters');
 xlabel('time seconds');
-y2 = (long-long(end))*6e6.*cos(lat);
 subplot(2,1,2),plot(t, y2);
 ylabel('dist meters');
 xlabel('time seconds');
+end;
 
 figure(10);
-subplot(2,1,1),plot(y,y2,'b');
+subplot(2,1,1),
+%plot(y2,y,'b');
+plot(tdx,tdy,'b');
+hold on;
+plot(0,0,'rx');
+hold off;
 xlabel('distance meters');
 ylabel('distance meters');
-subplot(2,1,2),plot(sqrt(y.*y+y2.*y2),alt,'b');
+subplot(2,1,2),plot(sqrt(tdx.*tdx+tdy.*tdy),alt,'b');
 xlabel('distance');
 ylabel('altitude');
 
@@ -79,8 +91,8 @@ ylabel('altitude meters');
 xlabel('time seconds');
 
 subplot(2,1,2),plot(t,speed);
-legend('speed');
 ylabel('meters/second');
+legend('speed');
 
 figure(3),
 subplot(2,1,1),plot(t,p, t,q,t,r);
@@ -89,6 +101,7 @@ ylabel('radians/second');
 subplot(2,1,2),plot(t,rud, t,elev,t,ail);
 legend('rudder','elevator','aileron');
 
+if (0)
 figure(4),
 factor = 100; % max(abs(q))/ max(abs(elev));
 subplot(2,1,1),plot(t,elev*factor, t, q,t,dq*500, t, iq*10);
@@ -116,4 +129,4 @@ figure(7);
 plot(t, r, t,dr,t,ir);
 legend('r','dr','ir');
 
-
+end;
