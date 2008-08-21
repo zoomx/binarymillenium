15f539e73cd40308dd3e79c8a8b4e3ae7d50d0c1
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2008-08-21 14:42:15 +0000

    Adding db.xml that came from velodyne, raw_to_csv has option to output theta phi radius now.

--- trunk/processing/velodyne/raw_to_csv.py
+++ trunk/processing/velodyne/raw_to_csv.py
@@ -5,14 +5,20 @@ import sys
 
 def processBin1206(bin,outfile, rot,vert,dist,z_off,x_off):
 
+	sphereical = True
+
 	#output = array.array('f')
 	
 	for i in range(12):
 		new_rot = (bin[i*100+3]*255 + bin[i*100+2])/100.0
 	
-		lower_not_upper = -1;
-		if bin[i*100+1] == 238: lower_not_upper = 0;   # EE upper
-		if bin[i*100+1] == 221: lower_not_upper = 1;   # DD lower
+		lower_not_upper = -1
+		if bin[i*100+1] == 238: lower_not_upper = 0   # EE upper
+		if bin[i*100+1] == 221: lower_not_upper = 1   # DD lower
+
+		#if (i == 11):
+			#print(str(bin[1201]*256 + bin[1200]) + '\n')	
+			#print(str(bin[1205]) + ' ' + str(bin[1203]) + '\n')	
 
 		if lower_not_upper >= 0:
 			for j in range(32):
@@ -27,25 +33,31 @@ def processBin1206(bin,outfile, rot,vert,dist,z_off,x_off):
 				phi = vert[sensor_index]
 
 				r = new_dist + dist[sensor_index]
+				
+				if (not sphereical):
+					x = r*math.cos(theta/180.0*math.pi)*math.cos(phi/180.0*math.pi) + x_off[sensor_index]*math.cos(theta/180.0*math.pi)
+					y = r*math.sin(theta/180.0*math.pi)*math.cos(phi/180.0*math.pi) + x_off[sensor_index]*math.sin(theta/180.0*math.pi)
+					z = r*math.sin(phi/180.0*math.pi) + z_off[sensor_index]
 
-				x = r*math.cos(theta/180.0*math.pi)*math.cos(phi/180.0*math.pi) + x_off[sensor_index]*math.cos(theta/180.0*math.pi)
-				y = r*math.sin(theta/180.0*math.pi)*math.cos(phi/180.0*math.pi) + x_off[sensor_index]*math.sin(theta/180.0*math.pi)
-				z = r*math.sin(phi/180.0*math.pi) + z_off[sensor_index]
 
-
-				#print(str(sensor_index) + ', ' + str(theta) + ', ' + str(phi) + ', ' + str(r) + ', '\
-				#	+ str(new_i) + ', ' + str(x) + ', ' + str(y) + ', '+ str(z))
-				outfile.write(str(new_i) + ', ' + str(x) + ', ' + str(y) + ', '+ str(z) + '\n')
+					#print(str(sensor_index) + ', ' + str(theta) + ', ' + str(phi) + ', ' + str(r) + ', '\
+					#	+ str(new_i) + ', ' + str(x) + ', ' + str(y) + ', '+ str(z))
+					outfile.write(str(new_i) + ', ' + str(x) + ', ' + str(y) + ', '+ str(z) + '\n')
 					
 				#output.append(float(new_i))
 				#output.append(x)
 				#output.append(y)
 				#output.append(z)
-
+				
+				else:
+					outfile.write(str(theta) + ', ' + str(phi) + ', ' + str(r) + '\n');
+					
 		# TBD something strange is happening here, the binary file ends
 		# up about 6.5 times bigger than it ought to be (16 bytes per position)
 		# and the text ends up being smaller
 		#output.tofile(outfile)
+
+	return new_rot
 
 #if the source file is 98e6 bytes, then there will be 98e6/1206*12*32 = 30.5e6 lines of positions, which will be
 # a huge text file
@@ -55,8 +67,9 @@ print(len(sys.argv))
 
 f = open(sys.argv[1]) #'unit46monterey.raw')
 
+startind = int(sys.argv[2]) # 1
 
-startind = int(sys.argv[2]) #'unit46monterey.raw')
+outname = (sys.argv[3]) #'unit46monterey.raw')
 
 
 
@@ -64,7 +77,7 @@ bin = array.array('B')
 bin.fromfile(f, 1206)
 
 #fout = open('output0.bin','wb')
-fout = open('output0.csv','wb')
+fout = open(outname + '0.csv','wb')
 
 dbfile = open('db.csv','rb')
 
@@ -86,6 +99,7 @@ for row in db:
 
 i = 0
 j = 0
+old_rot = 0;
 while (len(bin) == 1206):
 	# each call here produces 12*32 new points, i will increment to about 79,000 before this is done
 	#if (i%100 == 0): print(i)
@@ -93,16 +107,19 @@ while (len(bin) == 1206):
 	# this will have 2604 1206 byte packets per second, so split files int 1 second files
 	
 	if (j >= startind): 
-		processBin1206(bin,fout, rotCor,vertCor,distCor,vertOffCor,horizOffCor)
-
-
+		new_rot = processBin1206(bin,fout, rotCor,vertCor,distCor,vertOffCor,horizOffCor)
+		
 	bin = array.array('B')
 	bin.fromfile(f, 1206)
 
-	i = i+1
-	if (i == 2604):
-		i = 0
+	#i = i+1
+	#if (i == 2604):
+	# start a new file every rotation
+	if (new_rot < old_rot):
+		#i = 0
 		j = j +1
-		#fout = open('output' + str(j) +'.bin','wb')
 		if (j >= startind): 
-			fout = open('output' + str(j) +'.csv','wb')
+			fout = open(outname + str(j) +'.csv','wb')
+			print(str(j) + ', ' + str(new_rot) + '\n');
+
+	old_rot = new_rot
