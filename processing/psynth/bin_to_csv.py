4dfbc3df90284248a80169f103ac8867c8332c76
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2008-09-06 14:43:23 +0000

    Supplying a range number to the script will cause to loop through a bunch of points_0_ files

--- processing/psynth/bin_to_csv.py
+++ processing/psynth/bin_to_csv.py
@@ -15,67 +15,78 @@ def checkfloats(fbin):
 
 
 
-fraw = open(sys.argv[1]) 
-
-filesize= os.path.getsize(sys.argv[1])
-
-bsize = 14
-offset = filesize - (int(filesize/bsize) -1)*bsize
 
 
-bin = array.array('B')
+numargs = len(sys.argv)
 
-#bin.byteswap()
+rng = 1
+if (numargs > 2):
+	rng = int(sys.argv[2])
 
-#fout = open(outname,'wb')
+for i in range(rng):
 
-# print the header
-bin = array.array('B')
-bin.fromfile(fraw, offset) # 15)
-#for j in range(15):
-#	sys.stdout.write(str(bin[j]) + '\t')
-#print('\n');				
-
-
-fbin = array.array('f')
-fbin.fromfile(fraw, 3)
-
-bin = array.array('H')
-bin.fromfile(fraw, 1)
-
-count = 0;
-while(True): 
-	#sys.stdout.write(str(count) + ',\t')
-
-	fbin.byteswap()
-
-	# in multipart files the header is very huge, I'm not sure how to parse it
-	# but I do know that it generates bad floats, so just screen for bad floats:
-
-	if checkfloats(fbin):
-		for j in range(3):
-			sys.stdout.write(str(fbin[j]) + ',\t')
-
-		bin.byteswap()
-		red = 	(bin[0] >> 11) & 0x1f
-		green = (bin[0] >> 5)  & 0x3f
-		blue =  (bin[0] >> 0)  & 0x1f
+	filename = sys.argv[1] 
+	if (rng > 1):
+		filename = filename + str(i) + '.bin'
+	
+	fraw = open(filename) 
+	filesize= os.path.getsize(filename)
 		
-		sys.stdout.write( str(red) + ',\t' + str(green) + ',\t' + str(blue) )	
-		#sys.stdout.write( ',\t' + str((bin[0] >> 5) & 0x1))
+	bsize = 14
+	offset = filesize - (int(filesize/bsize) -1)*bsize
 
-	#	sys.stdout.write("%#x \t" % v2)
-		sys.stdout.write('\n')
+
+	bin = array.array('B')
+
+	#bin.byteswap()
+
+	#fout = open(outname,'wb')
+
+	# print the header
+	bin = array.array('B')
+	bin.fromfile(fraw, offset) # 15)
+	#for j in range(15):
+	#	sys.stdout.write(str(bin[j]) + '\t')
+	#print('\n');				
 
 	fbin = array.array('f')
-
-	try:
-		fbin.fromfile(fraw, 3)
-	except: 
-		break
+	fbin.fromfile(fraw, 3)
 
 	bin = array.array('H')
 	bin.fromfile(fraw, 1)
 
-	count = count+1
-	
+	count = 0;
+	while(True): 
+		#sys.stdout.write(str(count) + ',\t')
+		fbin.byteswap()
+
+		# in multipart files the header is very huge, I'm not sure how to parse it
+		# but I do know that it generates bad floats, so just screen for bad floats:
+
+		if checkfloats(fbin):
+			for j in range(3):
+				sys.stdout.write(str(fbin[j]) + ',\t')
+
+			bin.byteswap()
+			red = 	(bin[0] >> 11) & 0x1f
+			green = (bin[0] >> 5)  & 0x3f
+			blue =  (bin[0] >> 0)  & 0x1f
+
+			sys.stdout.write( str(red) + ',\t' + str(green) + ',\t' + str(blue) )	
+			#sys.stdout.write( ',\t' + str((bin[0] >> 5) & 0x1))
+
+			#	sys.stdout.write("%#x \t" % v2)
+			sys.stdout.write('\n')
+
+		fbin = array.array('f')
+
+		try:
+			fbin.fromfile(fraw, 3)
+		except: 
+			break
+
+		bin = array.array('H')
+		bin.fromfile(fraw, 1)
+
+		count = count+1
+
