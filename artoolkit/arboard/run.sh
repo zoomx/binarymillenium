4e5760d9054c9646331359885c8ea77700bd63b5
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2009-02-16 16:18:42 +0000

    The ds downsample by a factor of 4 here.

--- artoolkit/arboard/run.sh
+++ artoolkit/arboard/run.sh
@@ -3,7 +3,7 @@
 echo $1
 
 #curl http://192.168.1.57/now.jpg > $1/images/test.jpg
-curl http://192.168.1.57/now.jpg > /dev/shm/test.jpg
+curl http://192.168.1.57/now.jpg?ds=4 > /dev/shm/test.jpg
 #$1/arlaser $1/images/test.jpg $1
 $1/arlaser /dev/shm/test.jpg $1
 
