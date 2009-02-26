46f71119fc727ff76b877abc0ce069a3849f62ee
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2009-02-26 04:29:41 +0000

    This branch will grab a network camera image, find markers in it, and then udp the marker info to a different app.

---
+++ artoolkit/arnetcam/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo $1
+
+#curl http://192.168.1.57/now.jpg > $1/images/test.jpg
+curl http://192.168.1.57/now.jpg?ds=2 > /dev/shm/test.jpg
+#$1/arlaser $1/images/test.jpg $1
+$1/arlaser /dev/shm/test.jpg $1
+
+
