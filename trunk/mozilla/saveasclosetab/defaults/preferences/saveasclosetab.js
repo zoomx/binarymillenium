401f49632136d51674628dce8fc2a2ecf106d21f
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2009-08-23 23:29:26 +0000

    Firefox addon to save image as and close tab from context menu.

---
+++ trunk/mozilla/saveasclosetab/defaults/preferences/saveasclosetab.js
@@ -0,0 +1,2 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.saveasclosetab@binarymillenium.description", "chrome://saveasclosetab/locale/saveasclosetab.properties");
