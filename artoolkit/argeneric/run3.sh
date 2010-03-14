6461f72d6bbea7bc1d234bfc1fed7e4b51a0ed6c
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2010-03-14 17:41:19 +0000

    Print out opengl matrix instead of artoolkit matrix.

--- artoolkit/argeneric/run3.sh
+++ artoolkit/argeneric/run3.sh
@@ -2,5 +2,5 @@
 
 #for i in `seq -w 1 553`; do ./arlaser /home/lucasw/own/prog/synced/frames_yaw_rpt/roll_yaw_000$i.png . $i >> yaw_rpt.csv; done
 #for i in `seq -w 1 554`; do ./arlaser /home/lucasw/own/prog/synced/frames_pitch_rpt/pitch_rpt_000$i.png . $i >> pitch_rpt.csv; done
-for i in `seq -w 1 815`; do ./arlaser /home/lucasw/own/prog/synced/frames_roll_rpt/roll_rpt_000$i.png . $i >> roll_rpt.csv; done
+for i in `seq -w 1 1267`; do ./arlaser images/sl_ar$i.tif.png . $i  >> sl_ar.csv; done
 
