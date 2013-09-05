c7972e3f68de9cb44dfdcc2721c40cb05baf99c9
binarymillenium <binarymillenium@3bc78312-1b2a-0410-bf3d-39cd8132870d>
2013-09-05 12:12:15 +0000

    Standard command line run script for processing 2.0.

---
+++ trunk/processing/astar/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#~/other/processing-2.0b9/processing-java --sketch=../${PWD##*/} --run --output=output --force
+~/other/processing-2.0/processing-java --sketch=../${PWD##*/} --run --output=output --force
+
+
